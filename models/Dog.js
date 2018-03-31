b978717013c538972e96a0f98a86b652a4fe6795
Linda Carey <lhcarey09@gmail.com>
2018-03-31 01:00:42 -0700

    updated doggie class with details about doggie

--- models/Dog.js
+++ models/Dog.js
@@ -1,8 +1,18 @@
 const mongoose = require('mongoose');
 
 const dogSchema = new mongoose.Schema({
-  name: String,
-  owner: String
+  picture:      String,
+  name:         String,
+  breed:        String,
+  weight:       String,
+  gender:       String,
+  age:          Date,
+  fixed:        String,
+  tempermeant:  String,
+  energy_level: String,
+  about:        String,
+
+
 }, { timestamps: true });
 
 let Dog = module.exports = mongoose.model('Dog', dogSchema);
