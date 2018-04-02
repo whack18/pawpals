2400ae8a40669434645a9d979e9af969ac98d5bd
James Pham <vnlegend@gmail.com>
2018-04-01 22:16:15 -0700

    aws s3 upload, not done.

--- app.js
+++ app.js
@@ -19,6 +19,10 @@ const expressValidator = require('express-validator');
 const expressStatusMonitor = require('express-status-monitor');
 const sass = require('node-sass-middleware');
 const zipcodes = require('zipcodes');
+const aws = require('aws-sdk');
+aws.config.region = 'us-west-1';
+const S3_BUCKET = process.env.S3_BUCKET;
+
 const multer = require('multer');
 
 const storage = multer.diskStorage({
@@ -158,6 +162,37 @@ app.post('/account/profile', passportConfig.isAuthenticated, upload.single('user
 app.post('/account/password', passportConfig.isAuthenticated, userController.postUpdatePassword);
 app.post('/account/delete', passportConfig.isAuthenticated, userController.postDeleteAccount);
 app.get('/account/unlink/:provider', passportConfig.isAuthenticated, userController.getOauthUnlink);
+
+/*
+ * Respond to GET requests to /sign-s3.
+ * Upon request, return JSON containing the temporarily-signed S3 request and
+ * the anticipated URL of the image.
+ */
+app.get('/sign-s3', (req, res) => {
+  const s3 = new aws.S3();
+  const fileName = req.query['file-name'];
+  const fileType = req.query['file-type'];
+  const s3Params = {
+    Bucket: S3_BUCKET,
+    Key: fileName,
+    Expires: 60,
+    ContentType: fileType,
+    ACL: 'public-read'
+  };
+
+  s3.getSignedUrl('putObject', s3Params, (err, data) => {
+    if(err){
+      console.log(err);
+      return res.end();
+    }
+    const returnData = {
+      signedRequest: data,
+      url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
+    };
+    res.write(JSON.stringify(returnData));
+    res.end();
+  });
+});
 
 app.get('/api/google-maps', apiController.getGoogleMaps);
 /**
